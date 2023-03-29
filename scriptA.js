a76c0a0c7611d432fe5286d8ce771146c840af74
hyegunnn <126951081+hyegunnn@users.noreply.github.com>
2023-03-29 10:56:10 +0800

    Update scriptA.js

--- scriptA.js
+++ scriptA.js
@@ -1,3 +1,16 @@
-function showMessage(message) {
-    alert(message);
-}
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    const formData = new FormData(form);
+    fetch('process.php', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+        alert(data.message);
+    })
+    .catch(error => console.error(error));
+});
