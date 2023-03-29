36cc9bb42a1a448c9629d0521c80c880f0e7839a
hyegunnn <126951081+hyegunnn@users.noreply.github.com>
2023-03-29 11:08:32 +0800

    Add files via upload

---
+++ processA.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Identify String or Number - Result</title>
+    <link rel="stylesheet" href="styleA.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Identify String or Number - Result</h1>
+        <?php
+            $input = $_POST['input'];
+
+            if (is_numeric($input)) {
+                echo "<p>The value entered, <strong>$input</strong>, is a number.</p>";
+            } else {
+                echo "<p>The value entered, <strong>$input</strong>, is a string.</p>";
+            }
+        ?>
+        <p><a href="index.php">&laquo; Back to form</a></p>
+    </div>
+</body>
+</html>
